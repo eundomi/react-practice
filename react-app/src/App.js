8634acf8229d9a3f3307de7157cc3c1ce6395675
EUMDOMI <sandy131712@gmail.com>
2022-01-04 15:52:51 +0900

    :memo: 컴포넌트화
    컴포넌트화

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,46 @@
 import "./App.css";
 
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="index.html">React</a>
+      </h1>
+    </header>
+  );
+}
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="1.html">html</a>
+        </li>
+        <li>
+          <a href="2.html">css</a>
+        </li>
+        <li>
+          <a href="3.html">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB!
+    </article>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <h1>Hello,React!</h1>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Article />
+    </>
   );
 }
 
