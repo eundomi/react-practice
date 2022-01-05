313a631fba48f96d7153b02cbd5700ab432c9acd
EUMDOMI <sandy131712@gmail.com>
2022-01-05 12:35:00 +0900

    :memo: header클릭 시 팝업창뜨는 이벤트 구현
    "클릭하였습니다."라는 팝업창 뜨는 이벤트

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 
-function Header() {
+function Header(props) {
+  function onClickHandler(evt) {
+    evt.preventDefault();
+    props.onChangeMode();
+  }
   return (
     <header>
       <h1>
-        <a href="index.html">React</a>
+        <a href="index.html" onClick={onClickHandler}>
+          React
+        </a>
       </h1>
     </header>
   );
@@ -40,10 +46,12 @@ function App() {
     { id: 2, title: "css", body: "css is ..." },
     { id: 3, title: "js", body: "js is ..." },
   ];
-
+  function changeModeHandler() {
+    alert("클릭하였습니다.");
+  }
   return (
     <>
-      <Header />
+      <Header onChangeMode={changeModeHandler} />
       <Nav data={topics} />
       <Article title="Welcome" body="Hello,React" />
     </>
